91d66ad896e0943eb8c5eb71227445a900b1ab7e
LighTec <ktech117@gmail.com>
2017-11-17 12:50:46 -0700

    first push for as4, skeleton code

---
+++ as4/rushhour.py
@@ -0,0 +1,40 @@
+#        R
+#        R
+#       R
+#       R
+#      R
+#      R
+#     R
+#     R
+#
+# By Kell Larson
+
+import sys
+
+# Returns the input of a given filename, by parsing the values into a 2d array.
+def getInput(fileName):
+	txt = []
+	for line in open(fileName, "r"):
+		temp = line.replace("\n", "")
+		txt.append(temp)
+	return txt
+
+print(getInput(sys.argv[1]))
+
+class Car
+	def __init__(self, something1, something2, etc):
+
+	def __str__(self):
+
+class Board
+	def __init__(self, someInitStuff):
+
+	def addCar(self):
+
+	def loadGame(self):
+
+	def gameOver(self):
+
+	def __str__(self):
+
+	def moveCar(self):
