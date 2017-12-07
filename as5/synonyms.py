ec5992770dd1dfc7d1ee51b6135a2a174bcfabf4
LighTec <ktech117@gmail.com>
2017-12-07 15:13:28 -0700

    semantic descriptors can now be built

--- as5/synonyms.py
+++ as5/synonyms.py
@@ -1,22 +1,31 @@
 # function headers and the norm function are written by:
 # Michael Guerzhoy. Last modified: Nov. 18, 2015.
 
-# Code
+# Implemented Methods and misc code written by:
+# Kell Larson. Last modified: Dec. 7, 2017.
 
-import math
+import re
 
 # A vector is a dictionary of words, like this: ["man" : 1, "i" : 3] and so on.
+def testSuite():
+	sampleSentences = [["i", "am", "a", "sick", "man"], ["i", "am", "a", "spiteful", "man"], ["i", "am", "an", "unattractive", "man"], ["i", "believe", "my", "liver", "is", "diseased"],["however", "i", "know", "nothing", "at", "all", "about", "my","disease", "and", "do", "not", "know", "for", "certain", "what", "ails", "me"]]
+	
+	#print(build_semantic_descriptors(sampleSentences))
 
-def main():
 	words = [('man', 1, {'derp' : 1, 'a' : 2}), ('i' , 6, {'derp' : 3, 'b' : 1})]
+	
+	fileNames = ["Swanns Way"]#, "War and Peace"]
+	print(build_semantic_descriptors_from_files(fileNames))
 	#print(cosine_similarity(words[0][2], words[1][2]))
 	
 	# This needs to work
-	print(cosine_similarity({"a": 1, "b": 2, "c": 3}, {"b": 4, "c": 5, "d": 6}))
-	
-	man = {"i": 3, "am": 3, "a": 2, "sick": 1, "spiteful": 1, "an": 1,"unattractive": 1}
-	liver = {"i": 1, "believe": 1, "my": 1, "is": 1, "diseased": 1}
-	print(cosine_similarity(man,liver))
+	#print(cosine_similarity({"a": 1, "b": 2, "c": 3}, {"b": 4, "c": 5, "d": 6}))
+	#sample_dict = {	"man" : {"i": 3, "am": 3, "a": 2, "sick": 1, "spiteful": 1, "an": 1,"unattractive": 1}, "liver" : {"i": 1, "believe": 1, "my": 1, "is": 1, "diseased": 1}}
+	#print(cosine_similarity(sample_dict["man"], sample_dict["liver"]))
+
+
+def main():
+	testSuite()
 
 def norm(vec):
 #Return the norm of a vector stored as a dictionary,
@@ -50,10 +59,63 @@ def cosine_similarity(vec1, vec2):
 	return (topNum / sqrtAmt)
 
 def build_semantic_descriptors(sentences):
-	print("Incomplete")
+	finalDict = {} # master list
+	
+	for currentSentence in sentences:
+		for word in currentSentence:
+			if word not in finalDict:
+				finalDict.update({word : {}})
+			for wordToAdd in currentSentence:
+				if wordToAdd != word:
+					if wordToAdd not in finalDict[word]:
+						finalDict[word].update({wordToAdd : 1})
+					else:
+						finalDict[word][wordToAdd] = (finalDict[word][wordToAdd] + 1)
+	return finalDict
 
 def build_semantic_descriptors_from_files(filenames):
-	print("Incomplete")
+	# -*- coding: utf-8 -*-
+	
+	sentences = []
+
+	for filename in filenames:
+		f = open(filename, "r", encoding="utf-8")
+		text = f.read()
+		f.close()
+
+		# Code from StackOverflow, tokenizing sentences without 
+		# breaking apart sentences due to name prefixes or other things.
+		# modified from source, removed the implemented 
+		# Link: https://stackoverflow.com/questions/4576077/python-split-text-on-sentences
+
+		# Pad the text with spaces at both ends
+		text = " " + text + "  "
+		# Remove the gutenberg website, causes parsing errors
+		text = text.replace("http://www.gutenberg.org"," ")
+		# Remove next line command
+		text = text.replace("\n"," ")
+		# if there 
+		if "”" in text:
+			text = text.replace(".”","”.")
+		if "!" in text:
+			text = text.replace("!\"","\"!")
+		if "?" in text:
+			text = text.replace("?\"","\"?")
+		text = text.replace(".","<stop>")
+		text = text.replace("?","<stop>")
+		text = text.replace("!","<stop>")
+		sentences = text.split("<stop>")
+		sentences = sentences[:-1]
+		sentences = [s.strip() for s in sentences]
+		
+		#END StackOverflow code
+
+	tokenSentences = []
+	for sentence in sentences:
+		tokenSentences.append(sentence.split())
+	builtDescriptors = build_semantic_descriptors(tokenSentences)
+	return builtDescriptors
+	
 
 def most_similar_word(word, choices, semantic_descriptors, similarity_fn):
 	print("Incomplete")
